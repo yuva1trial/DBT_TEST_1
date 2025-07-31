aa9dd691bee560a107023ab41626ce7c92315462
yuvasree2020@gmail.com <yuvasree2020@gmail.com>
2025-07-31 11:05:54 +0000

    commit

--- macros/log_audit_failure.sql
+++ macros/log_audit_failure.sql
@@ -1,18 +1,19 @@
-{% macro log_audit_failure(model_name, test_name, failure_reason) %}
-    {% set sql %}
-        insert into {{ target.schema }}.central_audit_log (
-            model_name,
-            test_name,
-            failure_reason,
-            logged_at
-        )
-        values (
-            '{{ model_name }}',
-            '{{ test_name }}',
-            '{{ failure_reason | replace("'", "''") }}',
-            current_timestamp()
-        )
-    {% endset %}
-
-    {% do run_query(sql) %}
+{% macro log_audit_failure_from_results(results) %}
+    {% for result in results if result.status != 'pass' and result.resource_type == 'test' %}
+        {% set sql %}
+            insert into {{ target.schema }}.central_audit_log (
+                model_name,
+                test_name,
+                failure_reason,
+                logged_at
+            )
+            values (
+                '{{ result.node.name }}',
+                '{{ result.unique_id }}',
+                '{{ result.message | default("Test failure") | replace("'", "''") }}',
+                current_timestamp()
+            )
+        {% endset %}
+        {% do run_query(sql) %}
+    {% endfor %}
 {% endmacro %}
